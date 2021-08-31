e4a5221c0497f12bcb2b82add2ca2ad85f67a994
Vengerov Dmitry <hardbeat34@gmail.com>
2021-09-01 01:51:34 +0300

    added call facade

--- lib/lib.rb
+++ lib/lib.rb
@@ -4,8 +4,7 @@ class Audio
     def partial_upload(filename) 
         File.open(filename) do |mp3|
             mp3.seek(-128, IO::SEEK_END)
-            # mp3.read
-            mp3
+            mp3.read
         end
     end
 
@@ -14,14 +13,31 @@ class Audio
     end
 end 
 
-class MetaFetcher
+class MetaReader
     def call(id3)
+        case id3.class
+        when 'string'
+            read_string(id3)
+        when 'file'
+            read_file(id3)
+        else
+            raise "Read is not avaulable"
+    end
+
+    private 
+
+    def read_file
+        # 
+    end 
+
+    def read_string (id)
         raise "No ID3 tags" if id3 !~ /^TAG/
+
         p id3.unpack('xxxA30A30A30A4A30C1')
         @title, @artist, @album, @year, @comment, @genre = id3.unpack('xxxA30A30A30A4A30C1')
         @comment, @track = @comment.unpack('Z*@28C1') if @comment =~ /\0.$/
     end
-  end
+end
 
 if __FILE__ == $0
     audio = Audio.new()
@@ -32,7 +48,5 @@ if __FILE__ == $0
     binding.pry
     p part_file.instance_methods
     p file.instance_methods
-    # id3 = MetaFetcher.new.call(part_file)
-    # p id3
 
   end
